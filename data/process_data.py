92a5a25a60b6c8f90b7423db50f8dfbac21f8bd3
Phoo Pyae Pyae Linn <linnphoopyaepyae@gmail.com>
2021-06-16 23:48:37 +0630

    upload process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,17 +1,44 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    #merge two datasets using the common 'id' and assign to df
+    df = messages.merge(categories, left_on = 'id', right_on = 'id', how = 'outer') 
+    return df
 
 def clean_data(df):
-    pass
+    #transform the categories column
+    categories = df['categories'].str.split(';', expand=True)
 
+    row = categories.iloc[0]
+    category_colnames = list(map(lambda x: x[:-2], row))
+    categories.columns = category_colnames
+
+    #create dummies for categories
+    for column in categories:
+        categories[column] = categories[column].str.strip().str[-1] #get 0 or 1 of string
+        categories[column] = pd.to_numeric(categories[column]) #string to numeric
+
+    # drop the original categories column from `df`
+    df = df.drop('categories', axis = 1)
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis = 1, join = 'inner')
+
+    #remove duplicates
+    # check number of duplicates
+    print('Number of duplicates' ,df.duplicated().sum())
+    # drop duplicates
+    df = df.drop_duplicates()
+    # check number of duplicates
+    print(df.duplicated().sum())
+    return df
 
 def save_data(df, database_filename):
-    pass  
-
+    engine = create_engine(database_filename)
+    df.to_sql('disasterResponseTbl', engine, index=False)
 
 def main():
     if len(sys.argv) == 4:
